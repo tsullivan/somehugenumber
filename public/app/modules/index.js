8db993ac176155d55d014dc5f36b32428f94ea08
Timothy Sullivan <timsullivanaz@gmail.com>
2015-09-07 13:26:12 -0700

    consolidate app js :(

--- public/app/modules/index.js
+++ public/app/modules/index.js
@@ -1,2 +1,63 @@
-require('./blog');
-angular.module('somehugenumberApp');
+var blogModel = require('models/blog');
+
+angular
+	.module('somehugenumberApp', ['ngRoute', 'ngSanitize'])
+
+	.controller('MainBlogController', function ($scope, $routeParams) {
+		$scope.$routeParams = $routeParams;
+	})
+
+	.config(function($routeProvider) {
+		$routeProvider
+			.when('/:postId/:postSlug/:postDate', {
+				controller: 'PostController',
+				templateUrl: 'post.html'
+			})
+			.otherwise({
+				controller: 'ListController',
+				templateUrl: 'list.html'
+			});
+	})
+
+	/*
+	 * List blog posts
+	 */
+	.controller('ListController', ['$scope', '$routeParams', '$http',
+			function ($scope, $routeParams, $http) {
+				$scope.$routeParams = $routeParams;
+
+				$scope.method = 'GET';
+				$scope.url = '/blog/posts';
+				$scope.params = '/blog/posts';
+
+				$http({method: $scope.method, url: $scope.url}).
+					then(function (response) {
+						$scope.status = response.status;
+						$scope.list = blogModel.list(response.data);
+					}, function (response) {
+						$scope.status = response.status;
+						$scope.error = blogModel.getError('Request failed');
+					});
+			}])
+
+	/*
+	 * Show single blog post
+	 */
+	.controller('PostController', ['$scope', '$routeParams', '$http', '$sce',
+			function ($scope, $routeParams, $http, $sce) {
+				$scope.$routeParams = $routeParams;
+
+				$scope.method = 'GET';
+				$scope.url = '/blog/posts/' + $routeParams.postId;
+
+				$http({method: $scope.method, url: $scope.url}).
+					then(function (response) {
+						$scope.status = response.status;
+						$scope.post = blogModel.post(response.data);
+						$scope.post.content = $sce.trustAsHtml(response.data.content);
+					}, function (response) {
+						$scope.status = response.status;
+						$scope.error = blogModel.getError('Request failed');
+					});
+			}]);
+
