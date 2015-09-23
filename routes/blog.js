81f21f3e016ffb2bf04ab66670045b6cf2219deb
Timothy Sullivan <tsullivan@elastic.co>
2015-09-22 22:31:14 -0700

    refactor blog route

--- routes/blog.js
+++ routes/blog.js
@@ -8,15 +8,20 @@ var express = require('express'),
 var router = express.Router();
 var postsCache = {}; // simple memory cache
 
-function loadCache() {
+function fetchAndCache(callback) {
 	request(model.getUrl(), function (err, response, body) {
-		var posts;
-
-		if (!err && response.statusCode == 200) {
-			// Retrive the data objects and cache them
-			posts = model.list(JSON.parse(body).posts);
-			postsCache = _.indexBy(posts, 'id');
+		if (err || response.statusCode !== 200) {
+			// oops, send back error
+			return callback(model.getError(err, response));
 		}
+
+		var posts = model.list(JSON.parse(body).posts);
+
+		// index and cache the successfully retrieved data
+		postsCache = _.indexBy(posts, 'id');
+
+		// pass back success data in callback
+		callback(null, postsCache);
 	});
 }
 
@@ -26,24 +31,17 @@ function loadCache() {
 router.get(/^\/posts\/(\d+)$/, function(req, res, next) {
 	var postId = req.params[0];
 
-	// if in the cache, return early
+	// if in the cache, send it
 	if (_.has(postsCache, postId)) {
 		return res.json(postsCache[postId]);
 	}
 
-	request(model.getUrl(postId), function (err, response, body) {
-		var post;
-
-		if (!err && response.statusCode == 200) {
-			// Retrive the data object and construct post object
-			post = model.post(JSON.parse(body));
-
-			// Create a model for the response and render as JSON
-			postsCache[postId] = post;
-			return res.json(post);
+	fetchAndCache(function (err, posts) {
+		if (err) {
+			return next(err);
 		}
 
-		return next(model.getError(err, response));
+		return res.json(posts[postId]);
 	});
 });
 
@@ -51,28 +49,31 @@ router.get(/^\/posts\/(\d+)$/, function(req, res, next) {
  * GET blog posts list
  */
 router.get('/posts', function(req, res, next) {
-	// after response is sent, make a API request and cache results
-	process.nextTick(loadCache);
+	function parseIndexedObject(obj) {
+		return _.chain(obj)
+			.sortBy('rawdate')
+			.reverse()
+			.value();
+	}
 
 	// serve from cache?
 	if (_.keysIn(postsCache).length) {
-		var cachedResponse = _.chain(postsCache).sortBy('rawdate').reverse().value();
-		return res.json(cachedResponse);
+		// defer refresh the cache
+		process.nextTick(fetchAndCache.bind(null, function (err) {
+			if (err) {
+				postsCache = {}; // handle err by emptying cache
+			}
+		}));
+
+		return res.json(parseIndexedObject(postsCache));
 	}
 
-	// serve directly from api response, or error
-	request(model.getUrl(), function (err, response, body) {
-		var posts;
-
-		if (!err && response.statusCode == 200) {
-			// Retrive the data objects
-			posts = JSON.parse(body).posts;
-
-			// Create a model for the response and render as JSON
-			return res.json(model.list(posts));
+	fetchAndCache(function (err, posts) {
+		if (err) {
+			return next(err);
 		}
 
-		return next(model.getError(err, response));
+		return res.json(parseIndexedObject(posts));
 	});
 });
 
